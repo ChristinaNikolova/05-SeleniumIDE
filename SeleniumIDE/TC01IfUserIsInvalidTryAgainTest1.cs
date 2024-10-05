97e75d606d45e7326069624a3c4256d1062abcce
christinanikolova <christinanikolova@github.com>
2024-10-05 10:08:33 +0300

    try to fix the pipeline

--- SeleniumIDE/TC01IfUserIsInvalidTryAgainTest1.cs
+++ SeleniumIDE/TC01IfUserIsInvalidTryAgainTest1.cs
@@ -21,7 +21,12 @@ public class TC01IfUserIsInvalidTryAgainTest
     [SetUp]
     public void SetUp()
     {
-        driver = new ChromeDriver();
+        var options = new ChromeOptions();
+        options.AddArgument("--headless");
+        options.AddArgument("--disable-gpu");  
+        options.AddArgument("--no-sandbox");   
+        options.AddArgument("--disable-dev-shm-usage");
+        driver = new ChromeDriver(options);
         js = (IJavaScriptExecutor)driver;
         vars = new Dictionary<string, object>();
     }
